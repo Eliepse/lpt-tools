c18e6c8dcb9fe5cd0f1414ff4fe50c1f06e9ba20
Élie Meignan <contact@eliepse.fr>
2019-06-30 11:10:38 +0800

    core: update workingGrid

--- app/WorkginGrids/GridTemplatePinyin.php
+++ app/WorkginGrids/GridTemplatePinyin.php
@@ -2,9 +2,10 @@
 
 namespace App\WorkginGrids;
 
+use Eliepse\WorkingGrid\Template\WithDrawingTutorial;
 use Eliepse\WorkingGrid\Template\WithPinyin;
 
-class GridTemplatePinyin extends GridTemplate implements WithPinyin
+class GridTemplatePinyin extends GridTemplate implements WithPinyin, WithDrawingTutorial
 {
     //
 }
