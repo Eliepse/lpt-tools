990629d0edee273382acbae687fefdf6917daf35
Elie Meignan <contact@eliepse.fr>
2020-04-10 21:42:59 +0200

    change: new welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,82 +1,25 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Les petits trilingues</title>
+	<title>Les petits trilingues - Outils</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-
-            <div class="content">
-                <h1 class="title m-b-md">
-                    LPT
-                </h1>
-
-                <div class="links">
-                    <a href="{{ route('workingGrid') }}">Chinese grid</a>
-                    <a href="{{ route('englishGrid') }}">English grid</a>
-                </div>
-            </div>
-        </div>
-    </body>
+	<!-- Fonts -->
+	<link href="{{ mix("css/styles.css") }}" rel="stylesheet" type="text/css">
+</head>
+<body class="page--welcome">
+<main class="welcome__main">
+	<div class="welcome__content">
+		<h1 class="welcome__title">les petits trilingues</h1>
+		<ul class="welcom__links">
+			<li><a href="http://www.lespetitstrilingues.com">Website</a>&nbsp;&rightarrow;</li>
+			<li><a href="{{ route('workingGrid') }}">Grid generator (cn)</a>&nbsp;&rightarrow;</li>
+			<li><a href="{{ route('englishGrid') }}">Grid generator (en)</a>&nbsp;&rightarrow;</li>
+		</ul>
+	</div>
+</main>
+</body>
 </html>
