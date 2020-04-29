aa330759708221a305da749b849a48b7f6c96f89
Élie Meignan <contact@eliepse.fr>
2020-04-29 09:33:51 +0200

    core: add vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,35 @@
+import Vue from "vue"
+import Vuex from "vuex"
+import ChineseGridComponent from "./components/ChineseGridComponent"
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+Vue.use(Vuex)
 
-require('./bootstrap');
+document.execCommand("defaultParagraphSeparator", false, null);
 
-//window.Vue = require('vue');
+const store = new Vuex.Store({
+	state: {
+		chineseCards: {
+			current: [],
+			cachedList: []
+		}
+	},
+	getters: {},
+	mutations: {
+		createCard() {
+			const card = {value: "", pinyin: ""}
+			this.state.chineseCards.current.push(card)
+			return card;
+		},
+		updateCard({card, value, pinyin}) {
+			//this.state.chineseCards.current.fin
+		}
+	},
+	actions: {},
+})
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.component('chinese-grid', ChineseGridComponent);
 
-//Vue.component('example-component', require('./components/ExampleComponent.vue'));
-//
-//const app = new Vue({
-//    el: '#app'
-//});
+new Vue({
+	el: '#app',
+	store
+});
