7350e9194b0a34237f6d19629807a80045ff5518
Élie Meignan <contact@eliepse.fr>
2020-05-02 10:19:05 +0200

    core: correct assets and deps

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix
-	.disableNotifications()
-	.js('resources/js/app.js', 'public/js')
-	.sass('resources/sass/app.scss', 'public/css')
+mix.disableNotifications();
+
+mix.js('resources/js/app.js', 'public/js')
 	.sass('resources/sass/styles.scss', 'public/css');
+
+if (mix.inProduction()) {
+	mix.version();
+}
