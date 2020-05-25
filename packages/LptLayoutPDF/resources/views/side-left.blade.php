eaecf8f339634a1455a84eae4a4a2b5b4d3aa11c
Élie Meignan <contact@eliepse.fr>
2020-05-25 18:57:11 +0200

    change: chinese and text modification

--- packages/LptLayoutPDF/resources/views/side-left.blade.php
+++ packages/LptLayoutPDF/resources/views/side-left.blade.php
@@ -9,49 +9,49 @@
 		Fiche de pré-inscription
 	</h1>
 
-	<h2>Cours</h2>
+	<h2>Cours 课程</h2>
 
 	<table class="infos">
 		<tbody>
 		<tr>
-			<td class="label">Nom</td>
+			<td class="label">Niveau 级别</td>
 			<td class="value">{{ $course->name }}</td>
 		</tr>
 		<tr>
-			<td class="label">Horaire</td>
+			<td class="label">Horaire 时间</td>
 			<td class="value">@lang("onboarding.days." . $schedule["day"]) - {{ $schedule["hour"] }} h</td>
 		</tr>
 		<tr>
-			<td class="label">Établissement</td>
+			<td class="label">Établissement 校区</td>
 			<td class="value">{{ $course->school }}</td>
 		</tr>
 		</tbody>
 	</table>
 
-	<h2>Étudiant</h2>
+	<h2>Étudiant 学生</h2>
 
 	<table class="infos">
 		<tbody>
 		<tr>
-			<td class="label">Prénom NOM</td>
+			<td class="label">Prénom NOM (中文名)</td>
 			<td class="value">
 				{{ $student->getFullname() }} @if(!empty($student->fullname_cn))({{ $student->fullname_cn }})@endif
 			</td>
 		</tr>
 		<tr>
-			<td class="label">Date de naissance</td>
+			<td class="label">Date de naissance 生日</td>
 			<td class="value">{{ $student->born_at->toDateString() }}</td>
 		</tr>
 		<tr>
-			<td class="label">Ville de résidence</td>
+			<td class="label">Ville de résidence 居住地</td>
 			<td class="value">{{ $student->city_code }}</td>
 		</tr>
 		<tr>
-			<td class="label">Parent Wechat ID</td>
+			<td class="label">Wechat d'un parent</td>
 			<td class="value">{{ $student->first_contact_wechat }}</td>
 		</tr>
 		<tr>
-			<td class="label">Contact emergency</td>
+			<td class="label">Contact d'urgence</td>
 			<td class="value">
 				{{ $student->first_contact_phone }}<br>
 				{{ $student->second_contact_phone }}<br>
@@ -63,7 +63,7 @@
 	<table class="price">
 		<tbody>
 		<tr>
-			<td>Reste à payer</td>
+			<td>Reste à payer 需支付</td>
 			<td class="price__tag">{{ $course->price }} €</td>
 		</tr>
 		</tbody>
--- packages/LptLayoutPDF/resources/views/side-right.blade.php
+++ packages/LptLayoutPDF/resources/views/side-right.blade.php
@@ -1,22 +1,33 @@
 <div class="right-side">
-	<h3>Paiement</h3>
+	<h3>Paiement 支付方式</h3>
 	<p class="paiement-infos">
-		Pour temriner votre inscription et vous assurer une place dans notre école,
-		vous pouvez régler par espèce ou par virement bancaire.<br>
-		<strong>Espèce :</strong> rencontrez-nous dans une de nos école
-		<span class="underlined">munis de ce document</span> et de la somme indiquée (merci de faire l'appoint).<br>
-		<strong>Virement bancaire :</strong> contacter notre école via <i>wechat</i> (QR Code ci-contre) et transmettez
-		directement ce document. Nous vous répondrons rapidement pour vous indiquer la marche à suivre.
+		<strong>Paiment à l'école :</strong> contactez-nous via wechat (QR code ci-contre)
+		pour prendre connaissance des horaires de réception de nos écoles.<br>
+		<strong>线下支付 :</strong> 请加右上方微信了解各校区接待报名的时间。<br>
+		<strong>Paiement par virement :</strong><br>
+		<strong>线上支付 :</strong><br>
+		<span style="font-family: monospace">
+			Les Petits Trilingues<br>
+			RIB：17515900000801085947747
+		</span>
 	</p>
-	<h3>Notes</h3>
+	<h3>Notes 注</h3>
 	<ul style="padding-left: 24px">
-		<li>Ce document ne fait pas office de devis, la prix peut varier selon les situations.</li>
-		<li>Ce document ne fait pas office de facture.</li>
-		<li>Ce document ne vous garantit pas une place dans le cours demandé.</li>
-		<li>Votre enfant sera officiellement inscrit à réception du paiement.</li>
+		<li>
+			Ce document ne fait pas office de devis, la prix peut varier selon les situations.<br>
+			本文件仅为预定课程凭证，如有变化请以实际报名费用为准。
+		</li>
+		<li>
+			Ce document ne fait pas office de facture.<br>
+			本文件不做报销凭证。
+		</li>
+		<li>
+			Votre enfant sera officiellement inscrit à réception du paiement.<br>
+			确认正式报名需收到交费凭证。
+		</li>
 	</ul>
 	<hr>
-	<h3>Nos adresses</h3>
+	<h3>Nos adresses 各校区地址</h3>
 	<table class="schools">
 		<tbody>
 		<tr>
