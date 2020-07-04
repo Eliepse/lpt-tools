1de922348972f90c3943f9e913aa84cc34ddc0f5
Élie Meignan <contact@eliepse.fr>
2020-07-04 23:12:24 +0200

    change: tutorial height relative to cell size

--- app/WorkginGrids/GridTemplateStrokes.php
+++ app/WorkginGrids/GridTemplateStrokes.php
@@ -6,5 +6,6 @@ use Eliepse\WorkingGrid\Template\WithDrawingTutorial;
 
 class GridTemplateStrokes extends GridTemplate implements WithDrawingTutorial
 {
-    //
+	// Makes the tutorial height relative to the cell size
+	public $tutorial_height = 0;
 }
--- app/WorkginGrids/GridTemplateStrokesPinyin.php
+++ app/WorkginGrids/GridTemplateStrokesPinyin.php
@@ -7,5 +7,6 @@ use Eliepse\WorkingGrid\Template\WithPinyin;
 
 class GridTemplateStrokesPinyin extends GridTemplate implements WithPinyin, WithDrawingTutorial
 {
-    //
+	// Makes the tutorial height relative to the cell size
+    public $tutorial_height = 0;
 }
