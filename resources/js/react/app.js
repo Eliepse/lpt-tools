083aac438b4cbf603147b94de5af9509d71b5e95
Élie Meignan <contact@eliepse.fr>
2021-05-05 22:45:28 +0200

    feat: react-router, hot module reload & main layout

--- resources/js/react/app.js
+++ resources/js/react/app.js
@@ -1,14 +1,30 @@
-import {Component} from "react";
-import {Button} from 'antd';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
-class App extends Component {
-	render() {
-		return (
-			<div style={{padding: "24px"}}>
-				<Button type="primary">Click me!</Button>
-			</div>
-		);
-	}
+import {Breadcrumb, Button, Layout, Menu} from 'antd';
+
+const {Header, Content, Footer} = Layout;
+import styles from "./app.module.scss";
+
+export default function App() {
+	return (
+		<Router>
+			<Layout className={styles.layout}>
+				<Header className={styles.header}>
+					<Menu mode="horizontal" className={styles.menu}>
+						<Menu.Item key="home">Home</Menu.Item>
+						<Menu.Item key="registrations">Inscription</Menu.Item>
+					</Menu>
+				</Header>
+				<Content className={styles.content} style={{padding: '0 50px'}}>
+					<Breadcrumb style={{margin: '16px 0'}}>
+						<Breadcrumb.Item>Home</Breadcrumb.Item>
+					</Breadcrumb>
+					<div className={styles.siteLayoutContent}>
+
+					</div>
+				</Content>
+				<Footer style={{textAlign: 'center'}}>Les Petits Trilingues - Created by Eliepse</Footer>
+			</Layout>
+		</Router>
+	);
 }
-
-export default App;
