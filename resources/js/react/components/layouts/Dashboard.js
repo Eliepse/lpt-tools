ef8979cf33a5b15188aba0c2e092464f380de776
Élie Meignan <contact@eliepse.fr>
2021-05-30 22:00:47 +0200

    change: new dashboard layout

--- resources/js/react/components/layouts/Dashboard.js
+++ resources/js/react/components/layouts/Dashboard.js
@@ -1,26 +1,18 @@
 import styles from '../../app.module.scss';
 import Navigation from '../common/Navigation';
-import {Breadcrumb, Layout} from 'antd';
 import PropTypes from 'prop-types';
-
-const {Header, Content, Footer} = Layout;
 
 function Dashboard({children}) {
 	return (
-		<Layout className={styles.layout}>
-			<Header className={styles.header}>
+		<div className={styles.layout}>
+			<header className={styles.header}>
 				<Navigation/>
-			</Header>
-			<Content className={styles.content} style={{padding: '0 50px'}}>
-				<Breadcrumb style={{margin: '16px 0'}}>
-					<Breadcrumb.Item>Home</Breadcrumb.Item>
-				</Breadcrumb>
-				<div className={styles.siteLayoutContent}>
-					{children}
-				</div>
-			</Content>
-			<Footer style={{textAlign: 'center'}}>Les Petits Trilingues - Created by Eliepse</Footer>
-		</Layout>
+			</header>
+			<div className={styles.content}>
+				{children}
+			</div>
+			<footer className={styles.footer}>Les Petits Trilingues - Created by Eliepse</footer>
+		</div>
 	);
 }
 
