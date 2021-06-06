658c4fb9d4f460616a90bc38899006c77f45d941
Élie Meignan <contact@eliepse.fr>
2021-06-06 10:39:31 +0200

    change: selectively import dom render

--- resources/js/react/index.js
+++ resources/js/react/index.js
@@ -1,14 +1,7 @@
 'use strict';
 
 import React from "react";
-import ReactDOM from "react-dom";
+import {render} from "react-dom";
 import App from "./app.js";
 
-ReactDOM.render(
-	(
-		//<React.StrictMode>
-			<App/>
-		//</React.StrictMode>
-	),
-	document.querySelector("#app"),
-);
+render(<App/>, document.querySelector("#app"));
